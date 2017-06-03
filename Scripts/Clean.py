94c2a1b913a7a8a06b42d52a86b6151875066223
Dongping Zhang <dpzhang@berkeley.edu>
2017-06-03 10:54:02 -0500

    fixed pandas version issue

--- Scripts/Clean.py
+++ Scripts/Clean.py
@@ -11,6 +11,7 @@ from util import *
 
 
 if __name__ == "__main__":
+    # load the data
     staxi_path = os.path.join(os.pardir, "Data/SubsetData5000/STaxiTrips.csv")
     output_path = os.path.join(os.pardir, "Data/SubsetData5000/CSTaxiTrips.csv")
     staxi = clean_data(staxi_path) 
@@ -27,24 +28,24 @@ if __name__ == "__main__":
 
     ##### 4. ratio of real (actual) path length over shortest path length (RRSL)#####
 
-    # export the processed dataset
-    staxi.to_csv(output_path, index = False, float_format='%.3f')
     # explanation: dividing actual distance by absolute distance
-    staxi['RRSL'] = staxi['miles'] / staxi['AbsDistance']
-    staxi['RRSL'] = staxi['RRSL'].apply(lambda x: round(x,3))
+    staxi['RRSL'] = round(staxi['miles'] / staxi['AbsDistance'], 3)
+    #staxi['RRSL'] = staxi['miles'] / staxi['AbsDistance']
+    #staxi['RRSL'] = staxi['RRSL'].apply(lambda x: round(x,3))
 
     ##### 5. Actual Velocity: Actual Distance / Trip Duration #####
     # unit: miles / hr
     # averaging all speed limit sign in the chicago region (2009)
     staxi['AvgVelocity'] = 23.7
-
     
     ##### 6. Ratio of real path travel time over shortest path travel time (RRST) #####
-    staxi['AbsTime'] = staxi['AbsDistance'] / staxi['AvgVelocity']
-    staxi['AbsTime']  = staxi['AbsTime'].apply(lambda x: round(x,3))
-    
-    staxi['RRST'] = staxi['seconds'] / staxi['AbsTime']
-    staxi['RRST'] = staxi['RRST'].apply(lambda x: round(x,3))
+    staxi['AbsTime'] = round(staxi['AbsDistance'] / staxi['AvgVelocity'], 3)
+    staxi['RRST'] = round(staxi['seconds'] / staxi['AbsTime'], 3)
+
+    #staxi['AbsTime'] = staxi['AbsDistance'] / staxi['AvgVelocity']
+    #staxi['AbsTime']  = staxi['AbsTime'].apply(lambda x: round(x,3))    
+    #staxi['RRST'] = staxi['seconds'] / staxi['AbsTime']
+    #staxi['RRST'] = staxi['RRST'].apply(lambda x: round(x,3))
     
     ##### 7. Time Period: 8 levels #####
     staxi['pickup_hr'] = staxi.pickup_time.apply(get_timePeriod)
@@ -60,3 +61,7 @@ if __name__ == "__main__":
 
     ##### 11. day: indication of day #####
     staxi['day'] = staxi.pickup_time.apply(get_day)
+
+
+    # export the processed dataset
+    staxi.to_csv(output_path, index = False, float_format='%.3f')
